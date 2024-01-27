c1758a39da128f26269f4714a8ef00aa4b229cfc
samwaseee <samwaseee@gmail.com>
2024-01-28 00:07:27 +0600

    upper bound lower bound

--- D_Fast_search.cpp
+++ D_Fast_search.cpp
@@ -10,6 +10,22 @@ int lb(int arr[] , int j ,int s)
     while(l<r-1)
     {
         m = (r+l)/2;
+        if(arr[m] < s )
+            l = m ;
+        else 
+            r = m ;
+    }
+
+    return r;
+}
+int ub(int arr[] , int j ,int s)
+{
+    int l=-1,r=j;
+    int m;
+
+    while(l<r-1)
+    {
+        m = (r+l)/2;
         //cout<<l<<" ";
         if(arr[m] <= s )
             l = m ;
@@ -24,21 +40,23 @@ int main()
 {
     int n,k;
 
-    cin>>n>>k;
+    cin>>n;
 
-    int a[n+2];
+    int a[n+5];
 
     for (int i = 0; i < n; i++)
     {
         cin>>a[i];
     }
+    sort(a,a+n);
+
+    cin>>k;
     
     while(k--)
     {
-        int x;
-        cin>>x;
+        int l ,r ;
+        cin>>l>>r;
 
-        cout<<lb(a,n,x);
-        cout<<endl;
+        cout<<ub(a,n,r)-lb(a,n,l)<<" ";
     }
 }
